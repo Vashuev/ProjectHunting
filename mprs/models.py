6f76f5f59e5740ffc6b1cf4b6fa6fc0d0bf9f5d3
Rohit Bisht <rohitbst.104764@gmail.com>
2022-06-07 18:46:14 +0530

    + lastModify, url, votedby, voteCount

--- mprs/models.py
+++ mprs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
-from datetime import datetime  
+from django.utils import timezone
 
 class NameField(models.CharField):
     def __init__(self, *args, **kwargs):
@@ -24,9 +24,14 @@ class ProjectModel(models.Model):
     subtitle = models.CharField(max_length=200, blank=True)
     description = models.CharField(max_length=500)
     posted_at = models.DateTimeField(auto_now_add=True)
+    last_modify = models.DateTimeField(auto_now=True)
     owner_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     upvote = models.PositiveIntegerField(default=0)
+    voteCount = models.PositiveIntegerField(default=0)
+    url = models.URLField(default='https://jsonplaceholder.typicode.com/')
     tag = models.ManyToManyField(TagModel, related_name="projects")
+    votedBy = models.ManyToManyField(get_user_model(), related_name='project_voted')
+
 
     def __str__(self) -> str:
         return self.name
@@ -39,14 +44,14 @@ class ScreenshotModel(models.Model):
 
 class CommentModel(models.Model):
     comment = models.TextField()
-    date = models.DateTimeField(auto_now=True)
+    last_modify = models.DateTimeField(auto_now=True)
     project_id = models.ForeignKey(ProjectModel, related_name="comments", on_delete=models.CASCADE)
     owner_id = models.ForeignKey(get_user_model(), related_name="user_comments", on_delete=models.SET_DEFAULT, default=1)
 
 
 class ReplyModel(models.Model):
     reply = models.CharField(max_length=500)
-    date = models.DateTimeField(auto_now=True)
+    last_modify = models.DateTimeField(auto_now=True)
     comment_id = models.ForeignKey(CommentModel,related_name="replies", on_delete=models.CASCADE)
     owner_id = models.ForeignKey(get_user_model(), related_name="user_replies", on_delete=models.SET_DEFAULT, default=1)
 
